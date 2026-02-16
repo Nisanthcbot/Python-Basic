829da167f375b810da474785b09c3c18d23c95e6
Nisanthcbot <nisanth.m@cbotsolution.com>
2026-02-16 13:53:31 +0530

    level 1 qus and ans

---
+++ day1.py
@@ -0,0 +1,39 @@
+# Variable And DataType 
+
+"""
+Docstring for day1
+
+1. Variable
+2. Datatype
+3. Variable Scope
+4. Type Casting & Checking
+
+"""
+# Variable 
+
+'''
+  is an container to store the value 
+'''
+
+
+#DataType
+
+'''
+  1.int() number eg no = 12
+  2.float() floating number Eg number = 12.22
+  3.string() String eg Name = "nisanth'
+  4. boolean() true or Flase
+  5. None() nil value x
+
+'''
+
+name = "nisanth"
+age = 22
+city = "Annur"
+
+print("My name Is :",name)
+print("My Age Is :",age)
+print("My Palce Is :",city)
+
+
+
