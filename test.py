0349885fb5af9950052b23bc77deb86a472ab548
Nisanth <nisanth.m@cbotsolution.com>
2026-02-16 22:22:30 +0530

    new task which is arithmetic operator and task

---
+++ test.py
@@ -0,0 +1,8 @@
+sq = int(input("Enter the number :"))
+power = sq**2
+cub = sq**3
+rem = sq%5
+
+print(f"Squre Root of {sq} is {power}")
+print(f"Cube Root of {sq} is {cub}")
+print(f"Remainder when divided by 5 : {rem}")
